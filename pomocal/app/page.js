c5440bff675eef1931145492fe82fb9f6d8b2fdf
Dao Ho <daoho2004@gmail.com>
2023-10-15 20:12:09 -0400

    Added Intro for landing page

--- pomocal/app/page.js
+++ pomocal/app/page.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
 
     <main>
-      <h1>placeholder</h1>
-      <p>placeholder</p>
+      <h1>Pomocalendar Project</h1>
+      <p>The pomodoro technique is a time management method involving working on tasks for 25 minutes followed by 5 minutes of break </p>
     </main>
     
   )
