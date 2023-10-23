5782ba89fa15cdcbcdf78d8c2bb1e6e796fc3b3b
royaboya <94084201+royaboya@users.noreply.github.com>
2023-10-23 17:54:29 -0400

    ran prettier

--- pomocal/app/components/TButton.js
+++ pomocal/app/components/TButton.js
@@ -1,53 +1,56 @@
-import React from 'react';
-{/* bg-[#F1F1F1] hover:bg-[#ff865b] */}
-
+import React from "react";
+{
+  /* bg-[#F1F1F1] hover:bg-[#ff865b] */
+}
 
 export const TButton = () => {
-    return (
-        <div className="h-1/5 w-full flex justify-center pt-2.5">
-            <button className="
+  return (
+    <div className="h-1/5 w-full flex justify-center pt-2.5">
+      <button
+        className="
                     box-border h-1/2 w-5/12 
                     transition transition ease-in-out delay-50 bg-[#F1F1F1] hover:scale-105 hover:bg-[#ff865b] duration-100
                     drop-shadow-xl
                     text-center 
                     rounded-tl-md rounded-bl-md rounded-tr-md rounded-br-md
                     text-black hover:text-white
-                    border-2 border-[#ff865b] hover:border-gray-500">
-                <h1 className="
+                    border-2 border-[#ff865b] hover:border-gray-500"
+      >
+        <h1
+          className="
                             text-2xl
                             font-inter
-                            font-bold">
-                    <center>
-                        Start/Stop
-                    </center>
-                </h1>
-            </button>
-        </div>
-    )
-}
-
+                            font-bold"
+        >
+          <center>Start/Stop</center>
+        </h1>
+      </button>
+    </div>
+  );
+};
 
 export const SkipButton = () => {
-    return (
-        <div className="h-1/5 w-full flex justify-center pt-2.5">
-            <button className="
+  return (
+    <div className="h-1/5 w-full flex justify-center pt-2.5">
+      <button
+        className="
                     box-border h-1/2 w-5/12 
                     transition transition ease-in-out delay-50 bg-[#F1F1F1] hover:scale-105 hover:bg-[#ff865b] duration-100
                     drop-shadow-xl
                     text-center 
                     rounded-tl-md rounded-bl-md rounded-tr-md rounded-br-md
                     text-black hover:text-white
-                    border-2 border-[#ff865b] hover:border-gray-500">
-                <h1 className="
+                    border-2 border-[#ff865b] hover:border-gray-500"
+      >
+        <h1
+          className="
                             text-2xl
                             font-inter
-                            font-bold">
-                    <center>
-                        Skip
-                    </center>
-                </h1>
-            </button>
-        </div>
-    )
-}
-
+                            font-bold"
+        >
+          <center>Skip</center>
+        </h1>
+      </button>
+    </div>
+  );
+};
--- pomocal/app/components/TimerBox.js
+++ pomocal/app/components/TimerBox.js
@@ -1,34 +1,43 @@
-import React from 'react';
-import { TButton } from './TButton';
-import { SkipButton } from './TButton';
+import React from "react";
+import { TButton } from "./TButton";
+import { SkipButton } from "./TButton";
 
 export const Timerbox = () => {
-    return (
-        <div className="
-                box-border w-full h-screen flex-row">
-            <div className="h-1/4 w-full flex justify-center">
-                <div className="
-                                box-border h-full w-5/6 
-                                bg-[#ff865b]
-                                drop-shadow-xl
-                                text-center 
-                                rounded-tl-lg rounded-bl-lg rounded-tr-lg rounded-br-lg
-                                text-white">
-                    <div className="flex box-border h-full items-center justify-center">
-                        <h1 className="
-                                            text-9xl
-                                            font-inter
-                                            font-bold">24:56
-                        </h1>
-                    </div>
-                </div>
-            </div>
-            <div className = "flex-col h-full w-full">
-            <TButton/>
-            <SkipButton/>
-            </div>
+  return (
+    <div
+      className="
+            box-border w-full h-screen flex-row"
+    >
+      <div className="h-1/4 w-full flex justify-center">
+        <div
+          className="
+                    box-border h-full w-5/6 
+                    bg-[#ff865b]
+                    drop-shadow-xl
+                    text-center 
+                    rounded-tl-lg rounded-bl-lg rounded-tr-lg rounded-br-lg
+                    text-white"
+        >
+          <div className="flex box-border h-full items-center justify-center">
+            <h1
+              className="
+                            text-9xl
+                            font-inter
+                            font-bold"
+            >
+              24:56
+            </h1>
+          </div>
         </div>
-    )
-}
+      </div>
+      <div className="flex-col h-full w-full">
+        <TButton />
+        <SkipButton />
+      </div>
+    </div>
+  );
+};
 
-{/* export default Timerbox; */}
+{
+  /* export default Timerbox; */
+}
