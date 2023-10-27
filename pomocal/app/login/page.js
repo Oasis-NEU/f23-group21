b2b5d08264af3cce2e63602d5238b31bb512154d
royaboya <94084201+royaboya@users.noreply.github.com>
2023-10-27 03:29:56 -0400

    remove old gcal testing file

--- pomocal/app/login/page.js
+++ pomocal/app/login/page.js
@@ -6,7 +6,6 @@ export default function login(){
     return(
         <div>
             <GoogleAuthButton></GoogleAuthButton>
-            <h1>not sure what to do yet</h1>
         </div>
     )
 };
